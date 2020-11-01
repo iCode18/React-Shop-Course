bdba047b3cc2acc93ca4e92fb84ee2521bbd673b
Afshin Alavi <afshinalavi@Afshins-MacBook-Pro.local>
2020-11-01 17:52:14 +0100

    rating added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
-    <Card ClassName='my-3 p-3 rounded'>
+    <Card className='my-3 p-3 rounded'>
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </a>
@@ -14,9 +15,10 @@ const Product = ({ product }) => {
           </Card.Title>
         </a>
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews}
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className='rating'>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 1
+              ? 'fas fa-star'
+              : value >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 2
+              ? 'fas fa-star'
+              : value >= 1.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 3
+              ? 'fas fa-star'
+              : value >= 2.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 4
+              ? 'fas fa-star'
+              : value >= 3.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 5
+              ? 'fas fa-star'
+              : value >= 4.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      {/* if there is any text then show it */}
+      <span>{text && text}</span>
+    </div>
+  )
+}
+//Default value
+Rating.defaultProps = {
+  color: '#f8e825',
+}
+//Check the properties type, error in console browser
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+}
+export default Rating
